13de8465200c6cc1a67bd338e4abe7650e0fc17b
aron <aronphong@gmail.com>
2020-02-21 06:38:00 -0800

    Add contact contents

--- src/contact.js
+++ src/contact.js
@@ -2,7 +2,20 @@ const renderContactContent = () => {
     const container = document.querySelector("#content");
 
     const contact = document.createElement("div");
+    contact.className = "contact";
 
+    const contactTitle = document.createElement("h1");
+    contactTitle.id = "contact-header";
+    contactTitle.textContent =  "Contact Us";
+
+    contact.appendChild(contactTitle);
+
+    const contactInfo = document.createElement("p");
+    contactInfo.className = "contact-info";
+    contactInfo.innerHTML = "1111 Broad Way <br> Sacramento, CA <br> (916)-111-1111";
+
+    contact.appendChild(contactInfo);
+    
     container.appendChild(contact);
 }
 
